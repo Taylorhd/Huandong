9d476e4521656917effdf6dc8aee80d204ee517e
e <2469458627@qq.com>
2020-01-11 13:39:59 +0800

    界面中的空白部分是fragment属性padding造成的，修改后，上下滑动没有被分割

--- app/src/main/java/com/ecut/huandong/MainActivity.java
+++ app/src/main/java/com/ecut/huandong/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
                 R.id.navigation_dashboard,R.id.navigation_home,R.id.navigation_comminity, R.id.navigation_sport, R.id.navigation_discover)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
-//        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
 //        Toolbar toolbar = (Toolbar) findViewById(R.id.toobar);
 //        setSupportActionBar(toolbar);
--- app/src/main/java/com/ecut/huandong/ui/home/HomeViewModel.java
+++ app/src/main/java/com/ecut/huandong/ui/home/HomeViewModel.java
@@ -9,19 +9,6 @@ import androidx.lifecycle.ViewModel;
  * @author Cora
  */
 public class HomeViewModel extends ViewModel {
-//
-////    private MutableLiveData<String> mText;
-//    private MutableLiveData<CardView> mCardView;
-//    public HomeViewModel() {
-//        mCardView = new MutableLiveData<>();
-////        mText.setValue("This is home fragment");
-//    }
-//
-//    public LiveData<CardView> getCard(){
-//        return mCardView;
-//    }
+
+
 }
-//    public LiveData<String> getText() {
-//        return mText;
-//    }
-//}
